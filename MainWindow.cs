ab16ca9a1d400dad024c0d319d1e6c3f7150304c
hamish-milne <hammil@theovermare.com>
2014-12-03 04:25:18 +0000

    Tried to get Mono.TextEditor working

---
+++ MainWindow.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Gtk;
+using Mono.TextEditor;
+
+public partial class MainWindow: Gtk.Window
+{
+
+	TextEditor textEditor;
+	public MainWindow () : base (Gtk.WindowType.Toplevel)
+	{
+		var options = new TextEditorOptions ();
+		options.ColorScheme = "Oblivion";
+		Mono.TextEditor.Highlighting.SyntaxModeService.LoadStylesAndModes (typeof(TextEditor).Assembly);
+
+		textEditor = new TextEditor (new TextDocument (), options);
+		Build ();
+		scrolledWindow.Child = textEditor;
+		textEditor.ShowAll ();
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+}
